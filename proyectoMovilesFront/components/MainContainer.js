18e112ab6ef8411fbacb0962486a83da5573e5e9
Brian Esquivel <19151684@aguascalientes.tecnm.mx>
2023-05-15 19:44:50 -0600

    drawer

--- proyectoMovilesFront/components/MainContainer.js
+++ proyectoMovilesFront/components/MainContainer.js
@@ -11,11 +11,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import HomeScreen from './Home';
 import Login from './Login';
-<<<<<<< HEAD
-import Crud from './Crud';
-=======
 import Register from './Register';
->>>>>>> a8fd088b3c109db846c89380b9f89d6a4c69fec7
+import Crud from '.Crud';
 
 
 
@@ -26,20 +23,13 @@ const Drawer = createDrawerNavigator();
 export default function MainContainer() {
     return (
         <NavigationContainer>
-<<<<<<< HEAD
             <Drawer.Navigator>
                 <Drawer.Screen name="LOGIN" options={{ headerShown: false }} component={Login} />
                 <Drawer.Screen name="HOME SCREEN" options={{ headerShown: false, title: 'WELCOME' }} component={HomeScreen} />
                 <Drawer.Screen name="Crud" options={{ headerShown: false }} component={Crud}/>
+                <Drawer.Screen name="register" options={{ headerShown: false }} component={Register}/>
             </Drawer.Navigator>
 
-=======
-            <Stack.Navigator>
-                <Stack.Screen name="LOGIN" options={{ headerShown: false }} component={Login} />
-                <Stack.Screen name="HOME SCREEN" options={{ headerShown: false, title: 'WELCOME' }} component={HomeScreen} />
-                <Stack.Screen name="Register" options={{ headerShown: false, title: 'Register' }} component={Register} />
-            </Stack.Navigator>
->>>>>>> a8fd088b3c109db846c89380b9f89d6a4c69fec7
         </NavigationContainer>
     )
 }
