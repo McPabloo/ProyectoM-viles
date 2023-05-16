b3cb054be83d620daf404627d36bfe1f71b66fcb
mascorroSaul <smascorro2001@gmail.com>
2023-05-15 18:25:27 -0600

    actualización Mascorro

--- proyectoMovilesFront/App.js
+++ proyectoMovilesFront/App.js
@@ -1,14 +1,62 @@
-import * as React from "react";
-import { Center, VStack } from "native-base";
-import MainContainer from "./components/MainContainer";
+import React from "react";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+} from "native-base";
+import NativeBaseIcon from "./components/NativeBaseIcon";
 import Login from "./components/Login";
-import { NativeBaseProvider } from "native-base";
+import { Platform } from "react-native";
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
 
 export default function App() {
   return (
     <NativeBaseProvider>
-       <MainContainer/>
+      <Center
+        _dark={{ bg: "blueGray.900" }}
+        _light={{ bg: "blueGray.50" }}
+        px={4}
+        flex={1}
+      >
+        <VStack space={5} alignItems="center">
+          <Login />
+          <Heading size="lg">Welcome to NativeBase</Heading>
+        </VStack>
+      </Center>
     </NativeBaseProvider>
   );
 }
 
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light"}
+        onToggle={toggleColorMode}
+        aria-label={
+          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+        }
+      />
+      <Text>Light</Text>
+    </HStack>
+  );
+}
